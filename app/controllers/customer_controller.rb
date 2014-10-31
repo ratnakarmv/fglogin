b7b6871ac4f6a891e1595d12b8011f4fd4d8770a
Taufiq Muhammadi <waruboy@yahoo.com>
2014-10-31 19:07:27 +0700

    controller: customer to customers

--- app/controllers/customer_controller.rb
+++
@@ -1,6 +0,0 @@
-class CustomerController < ApplicationController
-  before_action :authenticate_customer!
-  def show
-
-  end
-end
---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,7 @@
+class CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  def show
+    @customer = current_customer
+
+  end
+end
